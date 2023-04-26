d05a71af7e6afc3271462ef054efde6cb2c08ef0
Guilherme F. Sampaio <63022500+GuiFernandess7@users.noreply.github.com>
2023-04-26 22:24:46 +0000

    Update settings.py

--- CustomUserModel/CustomUserModel/settings.py
+++ CustomUserModel/CustomUserModel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pca=_*+l9$wr#+e754*%-sbxkq5njs$l2-s-q7(_$%*ubj1#r!'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
