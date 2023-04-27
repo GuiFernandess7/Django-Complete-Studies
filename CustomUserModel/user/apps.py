04477acbef6f793487669278d77c73b6fe25d5fd
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-04-27 18:57:09 -0300

    Revert "UserConfig modified"
    This reverts commit 5ab21f3d02a3c6c0b82c66e1cbc5456fdf7bfba0.

--- CustomUserModel/user/apps.py
+++ CustomUserModel/user/apps.py
@@ -1,14 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
+
 
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
-
-    def say_hi(self):
-        print("Hiya")
-
-    def ready(self):
-        User = get_user_model()
-        User.say_hi = self.say_hi
-
