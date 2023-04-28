bfc4f1f3e55477c00dcecb6347558cc1405c380a
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-04-27 21:05:09 -0300

    Extended information of an user added

--- ExtendUserFields/user/admin.py
+++ ExtendUserFields/user/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from user.models import UserProfile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+class AccountsUserAdmin(AuthUserAdmin):
+    inlines = [UserProfileInline]
+
+admin.site.unregister(User)
+admin.site.register(User, AccountsUserAdmin)
