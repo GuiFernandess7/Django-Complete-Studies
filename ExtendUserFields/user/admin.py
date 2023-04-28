34a92981a1c9884f98101dcd7808c69940d00813
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-04-27 21:44:29 -0300

    add_view and change_view added

--- ExtendUserFields/user/admin.py
+++ ExtendUserFields/user/admin.py
@@ -7,8 +7,17 @@ class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
 
+# When you define an Inline, you allow users to edit information on 
+# related models directly from the edit form of the main model (in this case, User model)
 class AccountsUserAdmin(AuthUserAdmin):
-    inlines = [UserProfileInline]
+    def add_view(self, *args, **kwargs):
+        self.inline = []
+        return super(AuthUserAdmin, self).add_view(*args, **kwargs)
+        
+    def change_view(self, *args, **kwargs):
+        self.inlines = [UserProfileInline]
+        return super(AccountsUserAdmin, self).change_view(*args, **kwargs)
+    #inlines = [UserProfileInline]
 
 admin.site.unregister(User)
 admin.site.register(User, AccountsUserAdmin)
