25033ff7e9cabfac1057c837c773909e0a48fe6d
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-04-28 19:25:09 -0300

    gitignore added

--- ExtendUserField2/ExtendUserField2/settings.py
+++ ExtendUserField2/ExtendUserField2/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from key import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m3!4h6ksj51jji1c21-#1@%o$o5cd7_o6!y#y$tdcr%2p6^te6'
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
